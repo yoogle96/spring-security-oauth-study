c3cbd2d9e34e9e22ee8d303830280f3e58878652
yoogle96 <dmltjs851@gmail.com>
2020-01-16 02:15:11 +0900

    first commit

---
+++ src/main/java/com/yoogel/study/StudyApplication.java
@@ -0,0 +1,13 @@
+package com.yoogel.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yoogel/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoogel.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
